9162a6c7dd2d6a3f799cc990ac2c44651b0d29d3
techpassion <techpassion.vn@gmail.com>
2016-11-26 17:43:57 +0700

    update index page

--- index.js
+++ index.js
@@ -36,13 +36,14 @@ app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  //res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+  res.sendFile(path.join(__dirname, '/public/welcome.html'));
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
 app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/welcome.html'));
+  
 });
 
 var port = process.env.PORT || 1337;
